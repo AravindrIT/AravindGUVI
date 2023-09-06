05fcecc12367786c8856ef83adb8af560d827c23
Aravind R <aravindofficial28@gmail.com>
2023-09-06 14:24:00 +0530

    last change

--- Pratice/script.js
+++ Pratice/script.js
@@ -1,13 +1,3 @@
-alert("Are you sure, Do you want to read the Bible???");
-async function fetchData() {
-    const apiUrl = "https://bible-api.com/john%203:16";
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      const parentEle=Object.values(data);
-      document.getElementById("box").innerText=parentEle;      
-    } catch (err) {
-    document.write(`Error fetching Data: ${err}`);
-      }
-}
-fetchData();
+(hello = () => {
+    console.log("Hello");
+}) ();
